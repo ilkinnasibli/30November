6b004847babb4c5b3af263ef18192c2ca2783569
ilkinnasibli <ilkinnasibli@mail.ru>
2022-11-30 01:32:00 +0400

    task

---
+++ Task/Task/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = {2, 3,5, 14, 59 };
+int FindAll(int num1) 
+{
+	//int arraysize = 0;
+	for (int i = 0; i < numbers.Length; i++)
+    {int sum =0;
+        int[] array = new int[2];
+
+        if (numbers[i] > num1) 
+		{
+			sum++;
+			//arraysize++;
+			//array[i] = numbers[i];
+        }
+       
+  //      foreach (var item in array)
+		//{
+		//	Console.WriteLine(item);
+           return sum;
+
+  //      }
+	}
+	return -1;
+}
+Console.WriteLine(FindAll(6));
